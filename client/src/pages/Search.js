7ee04166a80336659c2d035d827b4766032f3ec4
clarkdkyle <clarkdkyle@gmail.com>
2020-05-09 13:15:41 -0500

    search working properly

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Results from "../components/Results";
-// import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-// import { Link } from "react-router-dom";
-// import { Col, Row, Container } from "../components/Grid";
-// import { List, ListItem } from "../components/Wrapper";
 import Form from "../components/Form"
 
 class Search extends React.Component {
@@ -29,49 +25,39 @@ class Search extends React.Component {
   }
   // Loads all books and sets them to books
   searchBook = query => {
-    API.getBook()
-      .then(res => this.setState({ books: res.data.items.map(
-          bookData => this.makeBook(bookData))
-        }))
-      
-      .catch(err => console.log(err));
-  };
-
-  // // Deletes a book from the database with a given id, then reloads books from the db
-  // function deleteBook(id) {
-  //   API.deleteBook(id)
-  //     .then(res => loadBooks())
-  //     .catch(err => console.log(err));
-  // }
+    API.getBook(query)
+        .then(res => this.setState({ books: res.data.items.map(bookData => this.makeBook(bookData)) }))
+        .catch(err => console.error(err));
+};
 
   // Handles updating component state when the user types into the input field
-  handleInputChange(event) {
+  handleInputChange =event => {
     const { name, value } = event.target;
     this.setState({[name]: value});
-  };
+  }; 
 
   // When the form is submitted, use the API.saveBook method to save the book data
   // Then reload books from the database
-  handleFormSubmit(event) {
+  handleFormSubmit = event => {
     event.preventDefault();
-    this.searchBook(this.state.search)
-  };
+    this.searchBook(this.state.search);
+};
 
-render() {
-        return (
-            <div>
-                <Form
-                    search={this.state.search}
-                    handleInputChange={this.handleInputChange}
-                    handleFormSubmit={this.handleFormSubmit}
-                />
-                <div className="container">
-                    <h2>Results</h2>
-                    <Results books={this.state.books} />
-                </div>
+  render() {
+    return (
+        <div>
+            <Form
+                search={this.state.search}
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+            />
+            <div className="container">
+                <h2>Results</h2>
+                <Results books={this.state.books} />
             </div>
-        )
-    }
+        </div>
+    )
+}
 }
 
 export default Search;
